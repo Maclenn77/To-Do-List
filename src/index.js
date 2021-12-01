fbf1df642f0b770029e963dc30f833400ae95ead
Maclenn77 <maclenn77@gmail.com>
2021-12-01 11:31:57 -0600

    Create an index.js. Create an array with objects

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
+const tasksBoard = document.getElementById('todo-list');
+let task1 = {description: "Task One", completed: false, index: 0};
+let task2 = {description: "Task Two", completed: false, index: 1};
+let task3 = {description: "Task Three", completed: true, index: 2};
+let toDoList = [task1, task2, task3];
+
+
+function displayTasks() {
+    const li = document.createElement('li');
   
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -10,5 +17,10 @@ function component() {
   
     return element;
   }
+
+  let li = document.createElement('li');
+  let content = document.createTextNode("Test");
   
-  document.body.appendChild(component());
+  tasksBoard.appendChild(li.appendChild(content));
+  tasksBoard.insertAdjacentHTML('beforeend', '<li id="two">two</div>');
+  
