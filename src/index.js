d4d54faba04f7ad0eb7786d8ddc0ed854cd3eb8a
Maclenn77 <maclenn77@gmail.com>
2021-12-01 13:58:31 -0600

    Solve lint problems

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasksBoard = document.getElementById('todo-list');
 
 // Dummy objects
-let task1 = {description: "Task One", completed: false, index: 0};
-let task2 = {description: "Task Two", completed: false, index: 1};
-let task3 = {description: "Task Three", completed: true, index: 2};
+const task1 = { description: 'Task One', completed: false, index: 0 };
+const task2 = { description: 'Task Two', completed: false, index: 1 };
+const task3 = { description: 'Task Three', completed: true, index: 2 };
 
 // Array with objects
 
-let toDoList = [task1, task2, task3];
+const toDoList = [task1, task2, task3];
 
 function isChecked(task) {
   if (task === true) {
     return 'checked';
   }
-  else {
-    return '';
-  }
 }
 
 function displayTasks() {
-
-   // Lodash, now imported by this script
-    for (let i = 0; i < toDoList.length; i += 1) {
-      tasksBoard.insertAdjacentHTML('beforeend', `<li><input type="checkbox" ${isChecked(toDoList[i].completed)}>${toDoList[i].description}</li>`)
-    }
+  for (let i = 0; i < toDoList.length; i += 1) {
+    tasksBoard.insertAdjacentHTML('beforeend', `<li><input type='checkbox' ${isChecked(toDoList[i].completed)}>${toDoList[i].description}</li>`)
   }
+}
 
-  
-  displayTasks();
+displayTasks();
--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-      index: './src/index.js',
-    },
-    devServer: {
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
 
-        static: './dist',
+      static: './dist',
     
-      },
-   plugins: [
- 
-     new HtmlWebpackPlugin({
- 
-        template: './src/index.html'
- 
-     }),
- 
-   ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
     },
-    module: {
+  plugins: [
+ 
+   new HtmlWebpackPlugin({
+ 
+      template: './src/index.html'
+ 
+   }),
+ 
+ ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
 
-        rules: [
+      rules: [
     
-          {
+        {
     
-            test: /\.css$/i,
+          test: /\.css$/i,
     
-            use: ['style-loader', 'css-loader'],
+          use: ['style-loader', 'css-loader'],
     
-          },
+        },
     
-        ],
+      ],
     
-      },
-  };
+    },
+};
