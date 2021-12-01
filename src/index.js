ea6378369f416ea9f78a7af9ee8d5ac1814d39f4
Maclenn77 <maclenn77@gmail.com>
2021-12-01 11:56:51 -0600

    Display list dynamically

--- src/index.js
+++ src/index.js
@@ -2,25 +2,24 @@ import _ from 'lodash';
 import './style.css';
 
 const tasksBoard = document.getElementById('todo-list');
+
+// Dummy objects
 let task1 = {description: "Task One", completed: false, index: 0};
 let task2 = {description: "Task Two", completed: false, index: 1};
 let task3 = {description: "Task Three", completed: true, index: 2};
+
+// Array with objects
+
 let toDoList = [task1, task2, task3];
 
 
 function displayTasks() {
-    const li = document.createElement('li');
-  
+
    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+    for (let i = 0; i < toDoList.length; i += 1) {
+      tasksBoard.insertAdjacentHTML('beforeend', `<li>${toDoList[i].description}</li>`)
+    }
   }
 
-  let li = document.createElement('li');
-  let content = document.createTextNode("Test");
   
-  tasksBoard.appendChild(li.appendChild(content));
-  tasksBoard.insertAdjacentHTML('beforeend', '<li id="two">two</div>');
-  
+  displayTasks();
